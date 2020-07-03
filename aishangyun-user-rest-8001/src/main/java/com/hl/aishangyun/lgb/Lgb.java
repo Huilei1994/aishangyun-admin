5dcd9fca4f998cd12183453f999a27679873c4e3
mac <2991808620@qq.com>
2020-07-03 16:20:01 +0800

    这是刘国彬的分治

---
+++ aishangyun-user-rest-8001/src/main/java/com/hl/aishangyun/lgb/Lgb.java
@@ -0,0 +1,15 @@
+package com.hl.aishangyun.lgb;
+
+/**
+ * @ClassName Lgb
+ * @Description TODO
+ * @Date 2020/7/3 16:18
+ * @Created by 刘国彬
+ */
+
+
+public class Lgb {
+    public void gfhja(){
+        System.out.println("fhasj");
+    }
+}
